ce53286faaffb10264dc71b1b19f2fb5696f6bce
Caiokl3i <caio.victor.santos12@gmail.com>
2025-10-23 23:49:15 -0400

    chore: organize project structure (jsx and components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
+import SideBar from './components/SideBar'
+import Topbar from './components/Topbar'
+import Tabs from './components/Tabs'
+import Table from './components/Table'
 import './App.css'
 
 function App() {
 
   return (
     <div>
-
+      <div className='app'>
+      <SideBar />
+      <div className="main">
+        <Topbar />
+        <div className="content">
+          <h2>Violão</h2>
+          <Tabs />
+          <div className='card-grid'>
+            
+          </div>
+          <div className="table">
+            <h2>Relatório</h2>
+            <Table />
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,15 @@
+
+
+const Topbar = () => {
+
+    return (
+        <div className="topbar">
+            <div className="search-bar">
+                <input type="text" placeholder="Pesquisa" />
+            </div>
+            <div class="profile">👤</div>
+        </div>
+    )
+}
+
+export default Topbar
